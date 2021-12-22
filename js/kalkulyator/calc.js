e579e420d664f0e6c3da511f7ecb4d8d897e68e6
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2021-12-22 20:02:46 +0400

    java ikinci ders

--- js/kalkulyator/calc.js
+++ js/kalkulyator/calc.js
@@ -1,10 +1,52 @@
-// if kod 10dan kicikdirse 20den boyukdurse console.log versin sehvdir
-// else if kod 20den boyuk 30dan kicikdirse console.log versin duzdur
+// if kod >  0 && <= 50 console.log olsun sehv
+// if kod > 50 && <= 80 console.log olsun duzgun
+// if kod < 90 && > 100 console.log olsun orta
+// if kod < 0 || > 100 console.log olsun duzgun daxil olmayib
 
-kod = 25;
-if (kod < 10) {
-    console.log("sehvdir")
+// let kod = -8;
 
-} else if (kod > 20) {
-    console.log("duzdur")
+// if (kod > 0 && kod <= 50) {
+//     console.log("sehv")
+// }
+// if (kod > 50 && kod <= 80) {
+//     console.log("duzgun")
+// }
+// if (kod < 90 && kod > 100) {
+//     console.log("orta")
+// }
+// if (kod < 0 || kod > 100) {
+//     console.log("duzgun daxil olmayib")
+// }
+
+function cixma() {
+    let a = 5;
+    let b = 2;
+    let c = a - b
+    console.log(c)
+}
+
+function toplama() {
+    let a = 5;
+    let b = 2;
+    let c = a + b
+    console.log(c)
+}
+
+function vurma() {
+    let a = 5;
+    let b = 2;
+    let c = a * b
+    console.log(c)
+}
+
+function bolme() {
+    let a = 5;
+    let b = 2;
+    let c = a / b
+    console.log(c)
+}
+
+let emr = "+"
+if (emr = "+") {
+    toplama()
 }
