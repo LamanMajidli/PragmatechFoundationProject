a47b62f1d053abc4c33a408b8bd88b9b486d9cc0
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2021-12-22 16:51:30 +0400

    java ikinci ders

---
+++ js/kalkulyator/calc.js
@@ -0,0 +1,10 @@
+// if kod 10dan kicikdirse 20den boyukdurse console.log versin sehvdir
+// else if kod 20den boyuk 30dan kicikdirse console.log versin duzdur
+
+kod = 25;
+if (kod < 10) {
+    console.log("sehvdir")
+
+} else if (kod > 20) {
+    console.log("duzdur")
+}
