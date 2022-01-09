b39491857a3d562b614ebb5cc8e391cf84e28500
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-01-10 01:17:14 +0400

    js tasklar 3cuden basqa

--- Works/portfolio/css/primer.js/primer.js
+++ Works/portfolio/css/primer.js/primer.js
@@ -1,31 +1,47 @@
-// let a = document.querySelector('.bocek');
-// let sagasolaget = true;
+// biricni task
 
-// function sagaget() {
-//     a.style.left = window.innerWidth - 200 + 'px'
+// querysel button cagir,addeventlistener klik funk elave et,daxile elave et div ,app child ile body icine elave et,yeni div'e class adi verilir, .css kimi yaz
+
+// let button = document.querySelector("button")
+// let body = document.querySelector("body")
+
+// button.addEventListener("click", () => {
+//     let div = document.createElement("div")
+//     div.classList.add("box")
+//     body.appendChild(div)
+//     div.style.background = "black"
+//         div.style.left = "37.5%"
+//         div.style.top = "27.5%"
+
+// })
+// let a = document.querySelector(".test")
+
+// function btn() {
+//     let box = document.createElement("div")
+//     box.classList = "first"
+//     a.appendChild(box)
+
 // }
 
-// function solaget() {
-//     a.style.left = "0"
-// }
-// let a = document.querySelector('.like');
-
-// let sum = 0;
-
-// function plus() {
-//     sum += 2; //sum++
-//     document.querySelector(".like").innerHTML = sum;
-// }
-
-// function minus() {
-//     sum -= 2;
-//     document.querySelector(".like").innerHTML = sum;
+// // ikinci task
+// for , li dovr, index.addeventl,display none
+// let ul = document.querySelector("ul")
+// for (i = 0; i < ul.length; i++) {
+//     li.addEventListener("click", (li) => {
+//             return
+//             ul.removeChild("li")
+//             li.style.display = "none"
+//         }
+//     }
+// function silinsin(a) {
+//     let b = a
+//     b.remove()
 // }
 
-// function reset() {
-//     let sum = 0
-//     document.querySelector(".like").innerHTML = sum;
+// let ikinci = document.querySelectorAll(".ikinci")
+// for (let i = 0; i < ikinci.length; i++) {
+//     let button = ikinci[i].querySelector("button")
+//     button.onclick = function() {
+//         ikinci[i].remove()
+//     }
 // }
-
-
-// querysel button cagir,addeventlistener klik funk elave et,daxile elave et div ,app child ile body icine elave et,yeni div'e class adi verilir,
