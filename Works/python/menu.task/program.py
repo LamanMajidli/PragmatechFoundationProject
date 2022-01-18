af6144c716bab905648760b75176785ee839c474
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-01-19 01:25:05 +0400

    19.01 task

--- Works/python/menu.task/program.py
+++ Works/python/menu.task/program.py
@@ -1,9 +1,52 @@
-eded=560
-# daxil olunan ededin eded olub olmamasini yoxla
-# ededin nece reqemli oldugunu cap et
-# 1-den hemin ededlere qeder olan ededlerin cemi
-cem=0
-for x in range(560):
-    if x%7==0:
-        cem+=x
-print(cem)
+
+# for i in range(1,500):    
+#     print(i)   
+
+
+# 8-12 setirdeki kodlar 1-ci taskdir
+
+# cem=0
+# for i in range(1,500):
+#     if len(str(i))==2:
+#         cem=cem+i
+# print(cem)        
+     
+     
+# 17-21  setirdeki kodlar 2-ci taskdir
+   
+# cem=0
+# for i in range(1,500):
+#     if len(str(i))==3:
+#         cem=cem+i
+# print(cem)
+
+
+# 26-27 setirler 3cu taskdir
+
+# arr=['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+# print(len(arr))
+
+
+# 32-33 setirler 4-cu taskdir
+
+# arr=['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+# print(len(str(arr)))
+
+
+# 38-39 setirdekiler 5-ci taskdir
+
+# arr=['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+# print(len(max(arr)))
+
+
+#  45-46 setirler 6-cu taskdir 
+# (str -"startswith" yazilma)
+
+# arr=['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+# print(arr.startswith("m"))   
+
+
+
+
+
+
