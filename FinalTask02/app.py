2016c300328197f4447d59835406dece9171a67a
Lyaman <95300855+LamanMajidli@users.noreply.github.com>
2022-02-06 17:08:46 +0400

    finaltask02

---
+++ FinalTask02/app.py
@@ -0,0 +1,34 @@
+# task03 2ci task
+
+# def FindCountry(countryname):
+#        if countryname in olkeler.keys():
+#         cities = olkeler.get(countryname) 
+#         print(f'{countryname} -' ,cities)
+#         pass
+
+
+# task03 4cu task
+
+# def CountAllCities():
+#       count=0
+#       city=list(olkeler.values())
+# for x in range(len(olkeler.values())):
+#     count += len(city[x])       
+#     print(count)
+
+# pass
+
+
+# task03 1-ci task
+# from re import I
+# from countries import olkeler
+# city=input()
+# def FindCity(cityname):
+#    Countries = list(olkeler.keys())
+#    cities = list(olkeler.values())
+#    for x in range(len(Countries)):
+#     for y in range(len(cities)):
+#       if cityname in cities[y] and x==y:
+#        print(f'{cityname} -' ,Countries[x])
+
+#    pass
